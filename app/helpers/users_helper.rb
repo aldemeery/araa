762d538451fb4fe88002e9e179a0f0d95ce593b9
Osama Aldemeery <aldemeery@gmail.com>
2020-07-26 18:33:05 +0200

    Remove logic from views.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,20 @@
 module UsersHelper
+  def following_action_path(user)
+    if user != logged_user
+      return logged_user.follows?(user) ? unfollow_path(user) : follow_path(user)
+    end
+    ""
+  end
+
+  def action_icon(user)
+    if user != logged_user
+      return logged_user.follows?(user) ? "fa-close" : "fa-plus user-profile-add-icon"
+    end
+    ""
+  end
+
+  def followed(user)
+    logged_user.follows?(user) ? "followed" : ""
+  end
 end
+
